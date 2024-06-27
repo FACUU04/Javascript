8e6b5cc21d05ed6859c3829e6062ce9aaa582454
Facu <facundososa550@gmail.com>
2024-06-27 16:27:47 -0300

    Seguimos practicando con las condiciones y variables, implementamos el uso de llamado a la varible en un alert, comparaciones y mas

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ alert(mensajeDeERROR)
 let Nombre = prompt("Cual es tu nombre?")
 console.log("El nombre ingresado es =" + Nombre)
 
-let Edad = prompt("Ingrese su edad para continuar")
+alert(`Bienvenid@ ${Nombre}!`);
+
+let Edad = prompt(`Ingresa tu edad para continuar ${Nombre}`)
 console.log("La edad del usuario es:" + Edad)
 if (Edad >= 18){
     alert("Puedes continuar")
@@ -21,3 +23,35 @@ if (Edad >= 18){
 if (Edad <=17){
     alert("Acceso denegado")
 }
+
+let Diasemana = prompt("Que dia es hoy?");
+console.log("Hoy es:" + Diasemana);
+
+if(Diasemana === 'Sabado' | Diasemana === 'sabado'| Diasemana === 'Domingo' | Diasemana === 'domingo') {
+    alert("Buen fin de semana :)")
+} else {
+    alert("Buena semana! :)")
+}
+
+let Numero = prompt("Ingresa un numero");
+console.log("El numero ingresado es:" + Numero);
+
+if (Numero >=0){
+    alert("El numero es positivo")
+} else {
+    alert("El numero que ingresaste es negativo")
+}
+
+let Puntuacion = 85;
+
+if (Puntuacion >=100){
+    alert("FELICIDADES, GANASTE!!!")
+} else {
+    alert("ARG, estuviste cerca, intenta otra vez para ganar")
+}
+
+let Saldo = 350;
+console.log("El saldo actual es de $:" + Saldo);
+
+let Mensaje = `Tu saldo actual es de $${Saldo}` 
+alert (Mensaje);
